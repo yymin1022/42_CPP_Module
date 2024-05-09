138c9184e40e88e2ebe2a5664fe995769067b26c
yymin1022 <yymin1022@gmail.com>
2024-05-09 16:47:48 +0900

    Update CPP Module 00 : ex00

--- CPP Module 00/ex00/megaphone.cpp
+++ CPP Module 00/ex00/megaphone.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 
-int main()
-{
-	std::cout << "Hello, World!" << std::endl;
+void print_upper(const std::string &str){
+	for(size_t i = 0; i < str.length(); i++){
+		std::cout << static_cast<char>(std::toupper(str[i]));
+	}
+}
+
+int main(int argc, char **argv){
+	if(argc == 1){
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}else{
+		for(int i = 1; i < argc; i++){
+			print_upper(argv[i]);
+		}
+	}
+
+	std::cout << std::endl;
+
 	return 0;
 }
