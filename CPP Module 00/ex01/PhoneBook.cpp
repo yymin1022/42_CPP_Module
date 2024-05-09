5d317724ca43b1fdbc6c33c11d11e38fce9d3e2a
yymin1022 <yymin1022@gmail.com>
2024-05-09 22:43:41 +0900

    Update CPP Module 00 : ex01

--- CPP Module 00/ex01/PhoneBook.cpp
+++ CPP Module 00/ex01/PhoneBook.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook(): contactsIdx(0){}
+PhoneBook::PhoneBook(): contactsCnt(0), contactsIdx(0){}
 PhoneBook::~PhoneBook(){}
 
-bool PhoneBook::check_avail(std::string& str){
+bool PhoneBook::check_avail(const std::string& str){
 	bool is_space_only = true;
 
 	for(size_t i = 0; i < str.length(); i++){
@@ -21,6 +20,20 @@ bool PhoneBook::check_avail(std::string& str){
 		return false;
 	}
 	return true;
+}
+
+
+void PhoneBook::print_width_10(const std::string& str){
+	std::string tmp;
+	if(str.length() <= 10){
+		tmp = str;
+	}else{
+		tmp = str.substr(0, 9);
+		tmp.append(".");
+	}
+	std::cout << "|";
+	std::cout.width(10);
+	std::cout << tmp;
 }
 
 void PhoneBook::add_contact(){
@@ -41,9 +54,9 @@ void PhoneBook::add_contact(){
 	}
 	newContact.setLastName(input);
 
-	std::cout << "Enter Nick Name: ";
+	std::cout << "Enter Nickname: ";
 	while(!getline(std::cin, input) || !check_avail(input)){
-		std::cout << "Enter Nick Name: ";
+		std::cout << "Enter Nickname: ";
 		continue;
 	}
 	newContact.setNickname(input);
@@ -64,10 +77,51 @@ void PhoneBook::add_contact(){
 
 	this->contacts[this->contactsIdx] = newContact;
 	this->contactsIdx = (this->contactsIdx + 1) % 8;
+	if(this->contactsCnt < 8){
+		this->contactsCnt++;
+	}
 	std::cout << "Contact Added!" << std::endl;
 }
 
 void PhoneBook::search_contact(){
-	std::cout << "Contact Index : " << this->contactsIdx << std::endl;
-	std::cout << ">> " << this->contacts[this->contactsIdx].getPhoneNumber() << std::endl;
+	if(this->contactsCnt == 0){
+		std::cout << "No Contacts Added!" << std::endl;
+		return;
+	}
+
+	print_width_10("Index");
+	print_width_10("First Name");
+	print_width_10("Last Name");
+	print_width_10("Nickname");
+	std::cout << "|" << std::endl;
+
+	for(int i = 0; i < this->contactsCnt; i++){
+		std::stringstream ss;
+		ss << i;
+		print_width_10(ss.str());
+
+		Contact cur = this->contacts[i];
+		print_width_10(cur.getFirstName());
+		print_width_10(cur.getLastName());
+		print_width_10(cur.getNickName());
+		std::cout << "|" << std::endl;
+	}
+
+	std::cout << "Enter Index: ";
+	std::string input;
+	if(!getline(std::cin, input) || input.empty() || input.length() != 1 || !isdigit(input[0])){
+		std::cout << "Invalid Input!" << std::endl;
+		return;
+	}
+
+	int idx = strtol(input.c_str(), 0, 10);
+	if(idx < 0 || idx >= this->contactsCnt){
+		std::cout << "Invalid Input!" << std::endl;
+		return;
+	}
+	std::cout << "First Name: " << this->contacts[idx].getFirstName() << "\n";
+	std::cout << "Last Name: " << this->contacts[idx].getLastName() << "\n";
+	std::cout << "Nickname: " << this->contacts[idx].getNickName() << "\n";
+	std::cout << "Phone Number: " << this->contacts[idx].getPhoneNumber() << "\n";
+	std::cout << "Darkest Secret: " << this->contacts[idx].getDarkestSecret() << "\n";
 }
--- CPP Module 00/ex01/PhoneBook.hpp
+++ CPP Module 00/ex01/PhoneBook.hpp
@@ -1,14 +1,17 @@
 #ifndef PHONEBOOK_HPP
 # define PHONEBOOK_HPP
-
+# include <iostream>
+# include <sstream>
 # include "Contact.hpp"
 
 class PhoneBook
 {
 private:
+	int contactsCnt;
 	int contactsIdx;
 	Contact	contacts[8];
-	bool check_avail(std::string& str);
+	bool check_avail(const std::string& str);
+	void print_width_10(const std::string& str);
 public:
 	PhoneBook();
 	~PhoneBook();
