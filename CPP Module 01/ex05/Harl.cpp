5f54b68e0573edd8d532ccd85d553317dd13db01
yonyoo <yonyoo@student.42seoul.kr>
2024-05-13 02:47:18 +0900

    Update CPP Module 01 : ex05

--- CPP Module 01/ex05/Harl.cpp
+++ CPP Module 01/ex05/Harl.cpp
@@ -16,19 +16,19 @@ void Harl::complain(std::string level){
 	}
 }
 
-void Harl::debug(void){
+void Harl::debug(){
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special- ketchup burger. I really do!" << std::endl;
 }
 
-void Harl::info(void){
+void Harl::info(){
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void Harl::warning(void){
+void Harl::warning(){
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 
-void Harl::error(void){
+void Harl::error(){
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
