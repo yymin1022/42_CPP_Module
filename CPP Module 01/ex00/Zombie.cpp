e74eb11328a65678cc08312201a763db9cb0f086
yonyoo <yonyoo@student.42seoul.kr>
2024-05-12 23:04:26 +0900

    Update CPP Module 01 : ex00

--- CPP Module 01/ex00/Zombie.cpp
+++ CPP Module 01/ex00/Zombie.cpp
@@ -1,13 +1,13 @@
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name): _name(name){
-	std::cout << "Zombie " << _name << " is born" << std::endl;
+Zombie::Zombie(std::string name): name(name){
+	std::cout << "Zombie " << this->name << " is born" << std::endl;
 }
 
 Zombie::~Zombie(){
-	std::cout << "Zombie " << _name << " is dead" << std::endl;
+	std::cout << "Zombie " << this->name << " is dead" << std::endl;
 }
 
 void Zombie::announce(void){
-	std::cout << _name << " : BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->name << " : BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- CPP Module 01/ex00/Zombie.hpp
+++ CPP Module 01/ex00/Zombie.hpp
@@ -5,7 +5,7 @@
 
 class Zombie{
 private:
-	std::string _name;
+	std::string name;
 public:
 	Zombie(std::string name);
 	~Zombie();
