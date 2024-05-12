002c8ee48867d9da50a1aa4ba713e4f18c5d4036
yonyoo <yonyoo@student.42seoul.kr>
2024-05-12 23:07:21 +0900

    Update CPP Module 01 : ex02

---
+++ CPP Module 01/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(){
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "The memory address of the string variable : " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR : " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF : " << &stringREF << std::endl;
+	std::cout << "string: " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+}
