8ae14334cad26d0ef8955943fc435ed84eb90b44
yymin1022 <yymin1022@gmail.com>
2024-05-14 23:41:54 +0900

    Update CPP Module 02 : ex01

--- CPP Module 02/ex01/Fixed.cpp
+++ CPP Module 02/ex01/Fixed.cpp
@@ -27,14 +27,14 @@ Fixed& Fixed::operator=(const Fixed& obj){
 	std::cout << "Copy assignment operator called" << std::endl;
 
 	if(this != &obj)
-		value = obj.getRawBits();
+		this->value = obj.getRawBits();
 	return *this;
 }
 
 int Fixed::getRawBits() const{
 	std::cout << "getRawBits member function called" << std::endl;
 
-	return value;
+	return this->value;
 }
 
 void Fixed::setRawBits(int const raw){
@@ -44,11 +44,11 @@ void Fixed::setRawBits(int const raw){
 }
 
 int Fixed::toInt() const{
-	return value >> bits;
+	return this->value >> this->bits;
 }
 
 float Fixed::toFloat() const{
-	return static_cast<float>(value) / (1 << bits);
+	return static_cast<float>(this->value) / (1 << this->bits);
 }
 
 std::ostream& operator<<(std::ostream& os, const Fixed& obj){
