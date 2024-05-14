bdac1981a690062c31b21d3df12387ea425c732b
yymin1022 <yymin1022@gmail.com>
2024-05-14 23:38:35 +0900

    Update CPP Module 02 : ex02

--- CPP Module 02/ex02/Fixed.cpp
+++ CPP Module 02/ex02/Fixed.cpp
@@ -31,6 +31,84 @@ Fixed& Fixed::operator=(const Fixed& obj){
 	return *this;
 }
 
+bool Fixed::operator>(const Fixed& obj) const{
+	return this->value > obj.getRawBits();
+}
+
+bool Fixed::operator<(const Fixed& obj) const{
+	return this->value < obj.getRawBits();
+}
+
+bool Fixed::operator>=(const Fixed& obj) const{
+	return this->value >= obj.getRawBits();
+}
+
+bool Fixed::operator<=(const Fixed& obj) const{
+	return this->value <= obj.getRawBits();
+}
+
+bool Fixed::operator==(const Fixed& obj) const{
+	return this->value == obj.getRawBits();
+}
+
+bool Fixed::operator!=(const Fixed& obj) const{
+	return this->value != obj.getRawBits();
+}
+
+Fixed Fixed::operator+(const Fixed& obj) const{
+	return Fixed(toFloat() + obj.toFloat());
+}
+
+Fixed Fixed::operator-(const Fixed& obj) const{
+	return Fixed(toFloat() - obj.toFloat());
+}
+
+Fixed Fixed::operator*(const Fixed& obj) const{
+	return Fixed(toFloat() * obj.toFloat());
+}
+
+Fixed Fixed::operator/(const Fixed& obj) const{
+	return Fixed(toFloat() / obj.toFloat());
+}
+
+Fixed& Fixed::operator++(void){
+	(this->value)++;
+	return *this;
+}
+
+Fixed Fixed::operator++(int){
+	Fixed tmp(*this);
+	operator++();
+	return tmp;
+}
+
+Fixed& Fixed::operator--(void){
+	(this->value)--;
+	return *this;
+}
+
+Fixed Fixed::operator--(int){
+	Fixed tmp(*this);
+	operator--();
+	return tmp;
+}
+
+Fixed& Fixed::min(Fixed& a, Fixed& b){
+	return a < b ? a : b;
+}
+
+const Fixed& Fixed::min(const Fixed& a, const Fixed& b){
+	return a < b ? a : b;
+}
+
+Fixed& Fixed::max(Fixed& a, Fixed& b){
+	return a > b ? a : b;
+}
+
+const Fixed& Fixed::max(const Fixed& a, const Fixed& b){
+	return a > b ? a : b;
+}
+
 int Fixed::getRawBits() const{
 	std::cout << "getRawBits member function called" << std::endl;
 
