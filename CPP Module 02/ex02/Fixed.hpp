a622de98840ecac0f1f503475d98c3bac7c2c668
yymin1022 <yymin1022@gmail.com>
2024-05-14 23:35:28 +0900

    Update CPP Module 02 : ex02

--- CPP Module 02/ex02/Fixed.hpp
+++ CPP Module 02/ex02/Fixed.hpp
@@ -14,7 +14,28 @@ public:
 	Fixed(int const value);
 	Fixed(float const value);
 	~Fixed();
+
 	Fixed& operator=(const Fixed& obj);
+	Fixed operator+(const Fixed& obj) const;
+	Fixed operator-(const Fixed& obj) const;
+	Fixed operator*(const Fixed& obj) const;
+	Fixed operator/(const Fixed& obj) const;
+	bool operator>(const Fixed& obj) const;
+	bool operator<(const Fixed& obj) const;
+	bool operator>=(const Fixed& obj) const;
+	bool operator<=(const Fixed& obj) const;
+	bool operator==(const Fixed& obj) const;
+	bool operator!=(const Fixed& obj) const;
+
+	Fixed& operator++();
+	Fixed operator++(int);
+	Fixed& operator--();
+	Fixed operator--(int);
+
+	static Fixed& min(Fixed& a, Fixed& b);
+	static Fixed& max(Fixed& a, Fixed& b);
+	static const Fixed& min(const Fixed& a, const Fixed& b);
+	static const Fixed& max(const Fixed& a, const Fixed& b);
 
 	int getRawBits() const;
 	void setRawBits(int const raw);
