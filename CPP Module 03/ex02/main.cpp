18beffc328b54c78fbebb0369ee595b31e552946
yonyoo <yonyoo@student.42seoul.kr>
2024-05-18 19:54:43 +0900

    Update CPP Module 03 : ex02

--- CPP Module 03/ex02/main.cpp
+++ CPP Module 03/ex02/main.cpp
@@ -2,6 +2,9 @@
 
 int main(){
 	FragTrap fragtrap1("Fragtrap");
+	FragTrap fragtrap2(fragtrap1);
+	FragTrap fragtrap3;
+	fragtrap3 = fragtrap1;
 
 	fragtrap1.attack("target");
 	fragtrap1.takeDamage(5);
