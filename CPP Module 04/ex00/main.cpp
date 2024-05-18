dab6f26b345788290b300eb26c13472d09017504
yymin1022 <yymin1022@gmail.com>
2024-05-19 02:22:01 +0900

    Update CPP Module 04 : ex00

---
+++ CPP Module 04/ex00/main.cpp
@@ -0,0 +1,28 @@
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongCat.hpp"
+
+int main(){
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the Cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	const WrongAnimal* meta2 = new WrongAnimal();
+	const WrongAnimal* j2 = new WrongCat();
+	std::cout << j2->getType() << " " << std::endl;
+	j2->makeSound();
+	meta2->makeSound();
+
+	delete meta;
+	delete j;
+	delete i;
+	delete meta2;
+	delete j2;
+
+	return 0;
+}
