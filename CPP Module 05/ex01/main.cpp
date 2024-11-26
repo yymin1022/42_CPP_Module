6e4dc6eac3da2d8393f525811e250a027461cab3
yymin1022 <yymin1022@gmail.com>
2024-11-26 22:09:41 +0900

    Update CPP Module 05 : ex01

--- CPP Module 05/ex01/main.cpp
+++ CPP Module 05/ex01/main.cpp
@@ -1,33 +1,25 @@
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int main(){
 	try{
-        Bureaucrat bureaucrat("Bob", 2);
-        std::cout << bureaucrat << std::endl;
-    }catch(Bureaucrat::GradeTooHighException &e){
-        std::cerr << e.what() << std::endl;
-    }catch(Bureaucrat::GradeTooLowException &e){
-        std::cerr << e.what() << std::endl;
+        Form form1("Form 1", 150, 150);
+        Bureaucrat bCrat1("Bob", 1);
+        form1.setSignature(bCrat1);
+        bCrat1.incrementGrade();
+    }catch(std::exception &e){
+        std::cout << e.what() << std::endl;
     }
 
-    try{
-        Bureaucrat bureaucrat1("Bob", 1);
-        bureaucrat1.incrementGrade();
-        std::cout << bureaucrat1 << std::endl;
-    }catch(Bureaucrat::GradeTooHighException &e){
-        std::cerr << e.what() << std::endl;
-    }catch(Bureaucrat::GradeTooLowException &e){
-        std::cerr << e.what() << std::endl;
-    }
+    std::cout << "---------" << std::endl;
 
     try{
-        Bureaucrat bureaucrat2("Bob", 150);
-        bureaucrat2.decrementGrade();
-        std::cout << bureaucrat2 << std::endl;
-    }catch(Bureaucrat::GradeTooHighException &e){
-        std::cerr << e.what() << std::endl;
-    }catch(Bureaucrat::GradeTooLowException &e){
-        std::cerr << e.what() << std::endl;
+        Form form1("Form 1", 1, 1);
+        std::cout << form1;
+        Bureaucrat bCrat1("Bob", 150);
+        form1.setSignature(bCrat1);
+    }catch(std::exception &e){
+        std::cout << e.what() << std::endl;
     }
 
 	return 0;
