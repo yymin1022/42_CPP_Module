f06f8ebf91f3617effc1379bc5a40b111475f6ac
yonyoo <yonyoo@student.42seoul.kr>
2024-11-29 07:26:32 +0900

    Update CPP Module 07 : ex01

--- CPP Module 07/ex01/iter.hpp
+++ CPP Module 07/ex01/iter.hpp
@@ -1,9 +1,17 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template<typename T>
+template<typename T1>
 
-void iter(T *array, size_t arr_len, void (*foo)(T&)){
+void iter(T1 *array, size_t arr_len, void (*foo)(T1&)){
+	if(array == NULL || foo == NULL)
+		return;
+	for(size_t i = 0; i < arr_len; i++)
+		foo(array[i]);
+}
+
+template<typename T2>
+void iter(const T2 *array, size_t arr_len, void (*foo)(const T2&)){
 	if(array == NULL || foo == NULL)
 		return;
 	for(size_t i = 0; i < arr_len; i++)
