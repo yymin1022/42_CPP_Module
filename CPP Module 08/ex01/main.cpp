cd6de5418a60b7d4f58c291bdfc4ed38270adcdb
yymin1022 <yymin1022@gmail.com>
2024-11-29 04:08:13 +0900

    Update CPP Module 08 : ex01

--- CPP Module 08/ex01/main.cpp
+++ CPP Module 08/ex01/main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 
-#include "whatever.hpp"
+#include "Span.hpp"
 
 int main(){
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
 
 	return 0;
 }
