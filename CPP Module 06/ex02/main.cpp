256a253bc22c8dee53221f87c97250b5276115ff
yymin1022 <yymin1022@gmail.com>
2024-11-29 03:13:32 +0900

    Update CPP Module 06 : ex02

--- CPP Module 06/ex02/main.cpp
+++ CPP Module 06/ex02/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
-int main(){
+#include "Base.hpp"
+#include "Identify.hpp"
 
+int main(){
+	try{
+		Base* first = generate();
+		Base* second = generate();
+		Base* third = generate();
+
+		identify(first);
+		identify(second);
+		identify(third);
+
+		identify(*first);
+		identify(*second);
+		identify(*third);
+
+		delete first;
+		delete second;
+		delete third;
+	}catch(const std::bad_alloc& e){
+		std::cerr << e.what() << '\n';
+		std::exit(EXIT_FAILURE);
+	}
 
 	return 0;
 }
