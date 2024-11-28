d45e86943a3aca81749fee191d312c0cb6498841
yymin1022 <yymin1022@gmail.com>
2024-11-29 04:21:41 +0900

    Update CPP Module 08 : ex01

--- CPP Module 08/ex01/main.cpp
+++ CPP Module 08/ex01/main.cpp
@@ -3,14 +3,26 @@
 #include "Span.hpp"
 
 int main(){
-	Span sp = Span(5);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	{
+		Span a = Span(10000);
+		a.addNumber(10000, time(NULL));
+		std::cout << a.shortestSpan() << std::endl;
+		std::cout << a.longestSpan() << std::endl;
+	}
 
 	return 0;
 }
