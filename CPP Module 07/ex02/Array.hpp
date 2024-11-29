5e7341ebc2d4f79876e5f55c97480fc0425e1613
yymin1022 <yymin1022@gmail.com>
2024-11-29 15:02:39 +0900

    Update CPP Module 07 : ex02

--- CPP Module 07/ex02/Array.hpp
+++ CPP Module 07/ex02/Array.hpp
@@ -1,74 +1,72 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
-template<typename T>
+#include <iostream>
+#include <exception>
 
-class Array{
+template<typename T>
+class Array {
 private:
-	T *_array;
-	size_t _size;
+    T* _array;
+    size_t _size;
 
 public:
-	Array(): _size(0){
-		std::cout << "Default Constructor called: created empty Array of size 0" << std::endl;
-		this->_array = new T[this->_size];
-	}
+    Array(): _size(0){
+        std::cout << "Default Constructor called: created empty Array of size 0" << std::endl;
+        this->_array = new T[this->_size];
+    }
 
-	Array(size_t size): _size(size){
-		std::cout << "Constructor for an Array of size " << size << " called" << std::endl;
-		this->_array = new T[this->_size];
-	}
+    Array(size_t size): _size(size){
+        std::cout << "Constructor for an Array of size " << size << " called" << std::endl;
+        this->_array = new T[this->_size];
+    }
 
-	Array(const Array &src): _size(src.size()){
-		std::cout << "Copy Constuctor called" << std::endl;
-		this->_array = new T[src._size];
-		this->_size = src._size;
+    Array(const Array& src): _size(src.size()){
+        std::cout << "Copy Constructor called" << std::endl;
+        this->_array = new T[src.size()];
+        for(size_t i = 0; i < src.size(); i++)
+            this->_array[i] = src._array[i];
+    }
 
-		for(size_t i = 0; i < src._size; i++)
-			this->_array[i] = src._array[i];
-	}
+    ~Array(){
+        delete[] this->_array;
+    }
 
-	~Array(){
-		if(this->_array != NULL)
-			delete[] this->_array;
-	}
+    Array& operator=(const Array& src){
+        if(this != &src){
+            delete[] this->_array;
+            this->_size = src.size();
+            this->_array = new T[this->_size];
+            for (size_t i = 0; i < this->_size; i++)
+                this->_array[i] = src._array[i];
+        }
+        return *this;
+    }
 
-	Array& operator=(const Array &src){
-		if(this != src){
-			if(this->_array != NULL)
-				delete[] this->_array;
-			this->_size = src.size();
-			this->_array = new T[this->_size];
-			for(size_t i = 0; i < this->size(); i++)
-				this->_array[i] = src._array[i];
-		}
-		return (*this);
-	}
+    T& operator[](size_t index){
+        if(index >= this->_size){
+            std::cout << "index: " << index << std::endl;
+            throw Array<T>::OutOfIndexException();
+        }
+        return this->_array[index];
+    }
 
-	T& operator[](size_t index){
-		if(index >= this->_size || this->_array == NULL){
-			std::cout << "index: " << index << std::endl;
-			throw Array<T>::OutOfIndexException();
-		}
-		return (this->_array[index]);
-	}
+    const T& operator[](size_t index) const{
+        if(index >= this->_size){
+            std::cout << "index: " << index << std::endl;
+            throw Array<T>::OutOfIndexException();
+        }
+        return this->_array[index];
+    }
 
-	const T& operator[](size_t index) const{
-		if(index >= this->_size || this->_array == NULL){
-			std::cout << "index: " << index << std::endl;
-			throw Array<T>::OutOfIndexException();
-		}
-		return (this->_array[index]);
-	}
+    class OutOfIndexException: public std::exception{
+    public:
+        virtual const char* what() const throw();
+    };
 
-	class OutOfIndexException: public std::exception{
-	public:
-		virtual const char	*what() const throw();
-	};
-
-	size_t size() const{
-		return (this->_size);
-	}
+    size_t size() const {
+        return this->_size;
+    }
 };
 
 template<typename T>
