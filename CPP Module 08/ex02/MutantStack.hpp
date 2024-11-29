252b15747590ce3d502a6fc900cc9da5fa1c1374
yymin1022 <yymin1022@gmail.com>
2024-11-29 16:56:17 +0900

    Update CPP Module 08 : ex02

--- CPP Module 08/ex02/MutantStack.hpp
+++ CPP Module 08/ex02/MutantStack.hpp
@@ -1,39 +1,61 @@
-#pragma once
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
 
-#include <iostream>
 #include <stack>
 
-template <class T>
-
-class MutantStack: public std::stack<T>{
+template<typename T>
+class MutantStack: public std::stack<T>
+{
 public:
-	typedef std::stack<T> stack;
-	typedef typename stack::container_type container;
-	typedef typename container::iterator iterator;
+    MutantStack();
+    MutantStack(const MutantStack& copy);
+    ~MutantStack();
+    MutantStack& operator=(const MutantStack& copy);
 
-	MutantStack(): stack(){
-		std::cout << "Default Constructor called" << std::endl;
-	}
+    typedef typename std::deque<T>::iterator iterator;
 
-	MutantStack(const stack &src): stack(src){
-		std::cout << "Copy Constructor called" << std::endl;
-	}
-
-	~MutantStack(){
-		std::cout << "Deconstuctor called" << std::endl;
-	}
-
-	stack &operator=(const stack &src){
-		if (*this != src)
-			*this = src;
-		return (*this);
-	}
-
-	iterator begin(){
-		return (stack::c.begin());
-	}
-
-	iterator end(){
-		return (stack::c.end());
-	}
+    iterator begin();
+    iterator end();
+    const iterator begin() const;
+    const iterator end() const;
 };
+
+template<typename T>
+MutantStack<T>::MutantStack()
+	: std::stack<T>(){}
+
+template<typename T>
+MutantStack<T>::MutantStack(const MutantStack& copy)
+	: std::stack<T>(copy){}
+
+template<typename T>
+MutantStack<T>::~MutantStack(){}
+
+template<typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack& copy){
+    if (this != &copy)
+        std::stack<T>::operator=(copy);
+    return *this;
+}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin(){
+    return this->c.begin();
+}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end(){
+    return this->c.end();
+}
+
+template<typename T>
+const typename MutantStack<T>::iterator MutantStack<T>::begin() const{
+    return this->c.begin();
+}
+
+template<typename T>
+const typename MutantStack<T>::iterator MutantStack<T>::end() const{
+    return this->c.end();
+}
+
+#endif
