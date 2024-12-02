0b5ab7ef6856c0f21d67cc010e12a024cbb2d648
yymin1022 <yymin1022@gmail.com>
2024-12-03 01:57:57 +0900

    Update CPP Module 09 : ex02

--- CPP Module 09/ex02/PmergeMe.hpp
+++ CPP Module 09/ex02/PmergeMe.hpp
@@ -1,17 +1,42 @@
 #ifndef PMERGEME_HPP
-# define PMERGEME_HPP
+#define PMERGEME_HPP
 
+#include <cmath>
+#include <iostream>
 #include <list>
+#include <sstream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-class PmergeMe{
+class PmergeMe {
 private:
+    std::vector<std::vector<int>> v;
+    std::list<std::list<int>> l;
+
+    int getJacobsthal(int k);
+    int getSizeList();
+    int getSizeVector();
+    int binarySearchList(int val, int left, int right);
+    int binarySearchVector(int val, int left, int right);
+    void mergeElementList(int idx1, int idx2);
+    void mergeElementVector(int idx1, int idx2);
+    void insertList(int idx, int insertIdx);
+    void insertVector(int idx, int insertIdx);
+    void lastSortList(int idx);
+    void lastSortVector(int idx);
+
 public:
-	PmergeMe();
-	PmergeMe(const PmergeMe& obj);
-	PmergeMe operator=(const PmergeMe& obj);
-	~PmergeMe();
+    PmergeMe();
+    PmergeMe(int argc, char **argv);
+    PmergeMe(const PmergeMe &obj);
+    ~PmergeMe();
+    PmergeMe &operator=(const PmergeMe &obj);
+
+    std::list<int> getResultList();
+    std::vector<int> getResultVector();
+    void sortList();
+    void sortVector();
 };
 
 #endif
