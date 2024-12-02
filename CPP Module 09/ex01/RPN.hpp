5691cdd026da1d2ea5403e4e3908f5681dcddec9
yymin1022 <yymin1022@gmail.com>
2024-12-03 01:21:26 +0900

    Update CPP Module 09 : ex01

---
+++ CPP Module 09/ex01/RPN.hpp
@@ -0,0 +1,22 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <cctype>
+#include <stack>
+#include <string>
+
+class RPN{
+private:
+	std::stack<int> stk;
+
+public:
+	RPN();
+	RPN(const RPN& copy);
+	~RPN();
+	RPN& operator=(const RPN& copy);
+
+	int getResult();
+	void input(char c);
+};
+
+#endif
