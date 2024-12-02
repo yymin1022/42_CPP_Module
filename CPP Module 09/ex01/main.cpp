9335583d80e6298df2d50bb24d8a01b135325658
yymin1022 <yymin1022@gmail.com>
2024-12-03 01:28:14 +0900

    Update CPP Module 09 : ex01

--- CPP Module 09/ex01/main.cpp
+++ CPP Module 09/ex01/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv){
 	try{
 		if(argc != 2)
-			throw std::string("Please input one argument");
+			throw std::string("Error: No Argument");
 
 		RPN rpn;
 		std::string expr(argv[1]);
@@ -13,12 +13,15 @@ int main(int argc, char **argv){
 			if((i & 1) == 0)
 				rpn.input(expr[i]);
 			else if(expr[i] != ' ')
-				throw std::string("Error");
+				throw RPN::RpnException();
 		}
 
 		std::cout << rpn.getResult() << std::endl;
 	}catch(const std::string& e){
 		std::cerr << e << std::endl;
+		return 1;
+	}catch(const RPN::RpnException& e){
+		std::cerr << e.what() << std::endl;
 		return 1;
 	}
 
