80038257f47b6985493885fd61b30169cf982bda
yymin1022 <yymin1022@gmail.com>
2024-12-02 23:21:54 +0900

    Update CPP Module 09 : ex00

--- CPP Module 09/ex00/BitcoinExchange.hpp
+++ CPP Module 09/ex00/BitcoinExchange.hpp
@@ -3,4 +3,22 @@
 
 #include <iostream>
 
+class BitcoinExchange{
+private:
+	BitcoinExchange();
+
+	float intToFloat(std::string str);
+	int floatToInt(std::string str);
+	void readData(std::string line);
+	void verifyDate(int* days, int year, int month, int day);
+
+public:
+	BitcoinExchange(const char *file);
+	BitcoinExchange(const BitcoinExchange& copy);
+	~BitcoinExchange();
+	BitcoinExchange& operator=(const BitcoinExchange& obj);
+
+	float getRate(std::string date);
+};
+
 #endif
