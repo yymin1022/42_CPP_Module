38bdfb4040f2e2b1bca65af0005a61f29db2bf2f
yymin1022 <yymin1022@gmail.com>
2024-12-02 23:21:50 +0900

    Update CPP Module 09 : ex00

--- CPP Module 09/ex00/main.cpp
+++ CPP Module 09/ex00/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-
 #include "BitcoinExchange.hpp"
 
-int main(){
+int main(int argc, char** argv){
+	if(argc != 2){
+		std::cerr << "Error: usage: BitcoinExchange <filename>" << std::endl;
+		std::exit(1);
+	}
+
 	std::cout << "Hello, World!" << std::endl;
 
 	return 0;
